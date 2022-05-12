dcd554030495f0edfed2737818f537643aa92fe1
Victor-PC <filpsixi@mail.ru>
2022-05-12 15:13:52 +0300

    stash@{0}

---
+++ note_api/exceptions.py
@@ -0,0 +1,6 @@
+
+
+class MismatchNoteAuthor(Exception):
+    def __init__(self, id_from_instance, user_id):
+        self.text = f"Id пользователя в поле author, экземпляра Note, должен совпадать с id " \
+                    f"пользователя редактирующего! В модели автор id - {id_from_instance} != {user_id}"
