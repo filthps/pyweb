e5558cfea41300b8d449b546bde54a4d74704c77
Victor-PC <filpsixi@mail.ru>
2022-05-15 07:57:50 +0300

    Фронтенд. XHR-запрос

--- note_api/static/js/notes-list.js
+++ note_api/static/js/notes-list.js
@@ -1,24 +1,54 @@
 function notes_wall(link, token) {
-    function create_load_button() {
-        var d = document;
-        var button = d.createElement("")
-    };
-    function content_checker(path, csrf) {
-        function parse_content(durty_data) {
+    let path = document.location + link + '/';
+    let time = 0;
+    let page = 0;
+    let drop_timer;
+    function check_cur_url() {
+        function get_last_symbol(str) {
+            return str.slice(str.length - 2, -1);
+        }
+        let url = document.location.pathname;
+        let last_symbol = get_last_symbol(url);
+        last_symbol = last_symbol === "/" ? get_last_symbol() : last_symbol;
+        if (!isNaN(last_symbol)) {
+            window.location.pathname = url.slice(0, url.indexOf(last_symbol));
+        }
+    }
+    function xhr_request(path, data, callback, token) {
+        let loader = new XMLHttpRequest();
+        loader.open('POST', path, true);
+        loader.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        loader.setRequestHeader('X-CSRFToken', token);
+        loader.onload = function (event) {
+            if (loader.readyState === 4 && loader.status === 200) {
+                callback(loader.response);
+            }
+        }
+        loader.send(data ? JSON.stringify(data) : null);
+    }
+    load_button = function() {
+        let d = document;
+        let button = d.createElement("button");
+    }
+    function parse_content(durty_data) {
             return JSON.parse(durty_data);
         }
-        function insert_content(parsed_obj) {
-            //
+    function insert_content(parsed_obj) {
+        //
+    }
+    function content_checker(path, csrf) {
+        function body(data) {
+            insert_content(parse_content(data));
         }
-        let loader = new XMLHttpRequest('POST', function(event) {
-            insert_content(parse_content(event.data))
-        });
-        loader.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        let drop_timer;
         let timer_instance = setInterval(function() {
-            loader.send(null);
+            xhr_request(path, null, body, csrf);
             drop_timer = function() {clearInterval(timer_instance);timer_instance = null;};
             }, 15000);
+        xhr_request(path, null, body, csrf);
     }
-    content_checker(link, token);
+    check_cur_url(); // Если текущий url кончается числом
+    // и исключений на фронте к этому моменту не возникло, то перенаправляемся на
+    // спецально обученный url, где предусмотрена работа в режиме ajax
+    content_checker(path, token);
 }
+init_();
