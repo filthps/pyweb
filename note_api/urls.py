e37a89a0f15fc3a4650adaf85b112641c835c350
Victor-PC <filpsixi@mail.ru>
2022-06-01 13:29:56 +0300

    Страница создания note

--- note_api/urls.py
+++ note_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import NotesList, CheckNewNotes, NotesLoader, CreateNote, AboutPage
+from .views import NotesList, CheckNewNotes, NotesLoader, CreateNote, CreateNoteShort, AboutPage
 
 urlpatterns = [
     path('', NotesList.as_view(), name='notes-list'),
     path('all/check-notes/', CheckNewNotes.as_view(), name='find-new-notes'),
     path('all/load-notes/', NotesLoader.as_view(), name='load-notes'),
     path('create/', CreateNote.as_view(), name='create-note'),
+    path('create-js/', CreateNoteShort.as_view(), name='create-note-js'),
     path('about/', AboutPage.as_view(), name='about')
 ]
