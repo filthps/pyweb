41c046b0c235041002e9bd852e2f23c5c2afbe6f
Литовченко Виктор Иванович <VIKLIT@avalon.ru>
2022-06-03 18:37:03 +0300

    Агрегация в виджете

--- note_api/paginator.py
+++ note_api/paginator.py
@@ -1,7 +1,7 @@
 from rest_framework import pagination
 
 
-MAX_SIZE_NOTES = 16  # Максимальное количество notes в рамках одного запроса
+MAX_SIZE_NOTES = 10  # Максимальное количество notes в рамках одного запроса
 
 
 class NotePaginator(pagination.PageNumberPagination):
